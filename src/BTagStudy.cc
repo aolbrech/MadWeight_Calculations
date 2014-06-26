c153c34126ab8d33aaebd868ad381ba46370ef74
Annik <annik.olbrechts@cern.ch>
2014-06-26 17:22:04 +0200

    Classes updated and optimized for both options. Different output names used in analyzer code!

--- src/BTagStudy.cc
+++ src/BTagStudy.cc
@@ -153,7 +153,58 @@ void BTagStudy::CorrectJetCombi5Jets(int BHadrIndex, int BLeptIndex, int Quark1I
      }
 }
 
-void BTagStudy::ReturnTable(std::string NameOfOption[6], int WhichJets, int NrOptionsConsidered, ofstream &output){
+void BTagStudy::ReturnTable(std::string NameOfOption4Jets[6], std::string NameOfOption5Jets[6], int WhichJets, int NrOptionsConsidered, ofstream &output, int OptionOfInterest){
+	
+    //Values of WhichJets are the following:
+	// 0 = all 4 jets matched and compared
+	// 1 = only the b-jets matched and compared
+	// 2 = only the light jets matched and compared
+
+    std::string Title[3] = {" \\textbf{Option} (no $\\chi^{2}$ $m_{lb}$) & all 4 correct   & $\\geq$ 1 wrong       & $\\frac{s}{\\sqrt{b}}$ & $\\frac{s}{b}$ & non-matched \\\\", 
+			    " \\textbf{Option} (no $\\chi^{2}$ $m_{lb}$) & 2 b's good      & $\\geq$ 1 b wrong     & $\\frac{s}{\\sqrt{b}}$ & $\\frac{s}{b}$ & non-matched \\\\", 
+			    " \\textbf{Option} (no $\\chi^{2}$ $m_{lb}$) & 2 light good    & $\\geq$ 1 light wrong & $\\frac{s}{\\sqrt{b}}$ & $\\frac{s}{b}$ & non-matched \\\\"};
+
+    std::string Caption[3] = {" \\caption{Overview of correct and wrong reconstructed events for the different b-tags without the use of a $\\chi^{2}$ $m_{lb}$ - $m_{qqb}$ method} ", 
+			      " \\caption{Overview of correct and wrong reconstructed b-jets for the different b-tags without the use of a $\\chi^{2}$ $m_{lb}$ - $m_{qqb}$ method} ", 
+			      " \\caption{Overview of correct and wrong reconstructed light jets for the different b-tags without the use of a $\\chi^{2}$ $m_{lb}$ - $m_{qqb}$ method} "};
+
+    output << " \\begin{table}[!h] \n \\begin{tabular}{c|c|c|c|c|c} " << endl;
+    output << Title[WhichJets] << " \\hline " << endl;
+    for(int ii = 0; ii < NrOptionsConsidered; ii++){
+
+	if(NrOptionsConsidered == 1){
+	  ii = OptionOfInterest;
+	  std::cout << " Looking at option : " << ii << " (inside ==1 loop ) --> Should be = " << OptionOfInterest << std::endl;
+          int CorrectOnes5Jets[3] = {allFourJetsCorrectlyMatched5Jets[OptionOfInterest], twoBTagsCorrectlyMatched5Jets[OptionOfInterest] , twoLightJetsCorrectlyMatched5Jets[OptionOfInterest]};
+          int WrongOnes5Jets[3] = {atLeastOneWronglyMatched5Jets[OptionOfInterest], atLeastOneBTagWronglyMatched5Jets[OptionOfInterest], atLeastOneLightJetWronglyMatched5Jets[OptionOfInterest]};
+
+          // 5-jet output
+          output << NameOfOption5Jets[ii]                                           << " & " <<
+          CorrectOnes5Jets[WhichJets]                                               << " & " <<
+          WrongOnes5Jets[WhichJets]                                                 << " & " <<
+          float(CorrectOnes5Jets[WhichJets])/float(sqrt(WrongOnes5Jets[WhichJets])) << " & " <<
+          float(CorrectOnes5Jets[WhichJets])/float(WrongOnes5Jets[WhichJets])       << " & " <<
+          NotReconstructedEvent[ii]                                                 << " \\\\ " << endl;
+	} 
+	std::cout << " Looking at option : " << ii << " (outside == 1 loop ) " << std::endl;
+
+	int CorrectOnes[3] = {allFourJetsCorrectlyMatched[ii], twoBTagsCorrectlyMatched[ii]    , twoLightJetsCorrectlyMatched[ii]};
+	int WrongOnes[3]   = {atLeastOneWronglyMatched[ii],    atLeastOneBTagWronglyMatched[ii], atLeastOneLightJetWronglyMatched[ii]};
+
+        output << NameOfOption4Jets[ii]                                 << " & " <<
+        CorrectOnes[WhichJets]                                          << " & " <<
+        WrongOnes[WhichJets]                                            << " & " <<
+	float(CorrectOnes[WhichJets])/float(sqrt(WrongOnes[WhichJets])) << " & " <<
+	float(CorrectOnes[WhichJets])/float(WrongOnes[WhichJets])       << " & " <<
+	NotReconstructedEvent[ii]                                       << " \\\\ " << endl;
+    }
+    output << " \\end{tabular} " << endl;
+    output << Caption[WhichJets] << endl;
+    output << " \\end{table} \n " << endl;
+
+}
+
+/*void BTagStudy::ReturnTable5Jets(std::string NameOfOption4Jets[6], std::string NameOfOption5Jets[6], int WhichJets, ofstream &outputNrJets, int OptionOfInterest){
 	
     //Values of WhichJets are the following:
 	// 0 = all 4 jets matched and compared
@@ -169,22 +220,33 @@ void BTagStudy::ReturnTable(std::string NameOfOption[6], int WhichJets, int NrOp
 			      " \\caption{Overview of correct and wrong reconstructed light jets for the different b-tags without the use of a $\\chi^{2}$ $m_{lb}$ - $m_{qqb}$ method} "};
 
     std::cout << " In class ReturnTable !" << std::endl;
-    output << " \\begin{table}[!h] \n \\begin{tabular}{c|c|c|c|c|c} " << endl;
-    output << Title[WhichJets] << " \\hline " << endl;
-    for(int ii = 0; ii < NrOptionsConsidered; ii++){
+    outputNrJets << " \\begin{table}[!h] \n \\begin{tabular}{c|c|c|c|c|c} " << endl;
+    outputNrJets << Title[WhichJets] << " \\hline " << endl;
 
-	    int CorrectOnes[3] = {allFourJetsCorrectlyMatched[ii], twoBTagsCorrectlyMatched[ii]    , twoLightJetsCorrectlyMatched[ii]};
-	    int WrongOnes[3]   = {atLeastOneWronglyMatched[ii],    atLeastOneBTagWronglyMatched[ii], atLeastOneLightJetWronglyMatched[ii]};
+    int CorrectOnes[3]      = {allFourJetsCorrectlyMatched[OptionOfInterest],      twoBTagsCorrectlyMatched[OptionOfInterest]      , twoLightJetsCorrectlyMatched[OptionOfInterest]};
 
-            output << NameOfOption[ii]                                   << " & " <<
-            CorrectOnes[WhichJets]                                          << " & " <<
-            WrongOnes[WhichJets]                                            << " & " <<
-	    float(CorrectOnes[WhichJets])/float(sqrt(WrongOnes[WhichJets])) << " & " <<
-	    float(CorrectOnes[WhichJets])/float(WrongOnes[WhichJets])       << " & " <<
-	    NotReconstructedEvent[ii]                                       << " \\\\ " << endl;
-     }
-    output << " \\end{tabular} " << endl;
-    output << Caption[WhichJets] << endl;
-    output << " \\end{table} \n " << endl;
 
-}
+    int WrongOnes[3]      = {atLeastOneWronglyMatched[OptionOfInterest],      atLeastOneBTagWronglyMatched[OptionOfInterest],      atLeastOneLightJetWronglyMatched[OptionOfInterest]};
+    int WrongOnes5Jets[3] = {atLeastOneWronglyMatched5Jets[OptionOfInterest], atLeastOneBTagWronglyMatched5Jets[OptionOfInterest], atLeastOneLightJetWronglyMatched5Jets[OptionOfInterest]};
+
+    // 5-jet output
+    outputNrJets << NameOfOption5Jets[ii]                                     << " & " <<
+    CorrectOnes5Jets[WhichJets]                                               << " & " <<
+    WrongOnes5Jets[WhichJets]                                                 << " & " <<
+    float(CorrectOnes5Jets[WhichJets])/float(sqrt(WrongOnes5Jets[WhichJets])) << " & " <<
+    float(CorrectOnes5Jets[WhichJets])/float(WrongOnes5Jets[WhichJets])       << " & " <<
+    NotReconstructedEvent[ii]                                                 << " \\\\ " << endl;
+
+    // 4-jet output
+    outputNrJets << NameOfOption4Jets[ii]                                     << " & " <<
+    CorrectOnes4Jets[WhichJets]                                               << " & " <<
+    WrongOnes4Jets[WhichJets]                                                 << " & " <<
+    float(CorrectOnes4Jets[WhichJets])/float(sqrt(WrongOnes4Jets[WhichJets])) << " & " <<
+    float(CorrectOnes4Jets[WhichJets])/float(WrongOnes4Jets[WhichJets])       << " & " <<
+    NotReconstructedEvent[ii]                                                 << " \\\\ " << endl;
+
+    outputNrJets< " \\end{tabular} " << endl;
+    outputNrJets << Caption[WhichJets] << endl;
+    outputNrJets << " \\end{table} \n " << endl;
+
+}*/
