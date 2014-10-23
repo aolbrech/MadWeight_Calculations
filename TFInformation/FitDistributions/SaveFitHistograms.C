3850e4272310356a1301010e46b268e8784aa4cb
Annik <annik.olbrechts@cern.ch>
2014-10-23 18:57:19 +0200

    Macro to draw the fit distributions together in one stacked canvas

---
+++ TFInformation/FitDistributions/SaveFitHistograms.C
@@ -0,0 +1,205 @@
+{
+  //-------------------------------------------------------------------------------------//
+  //  Values which need to be changed when a different directory should be accessed !!   //
+  //-------------------------------------------------------------------------------------//
+  //ROOT file which should be considered:
+  string Directory = "FromTree";
+  //Parameters:
+  const int NrBins = 11;
+  string HistoBin[NrBins] = {"1","2","3","4","5","6","7","8","9","10","11"};
+  const int NrParams = 6;                                                   //Looking at doubleGaussian with 6 parameters!
+  string Param[NrParams] = {"a1","a2","a3","a4","a5","a6"};                 //Names of the fit parameters  --> Check whether this is also correct for the ROOT class!
+  bool GetMkdirOutput = false;                                              //Output for directories which need to be created wanted ??
+  const int NrFitHistos = 12;                                               //Number of histograms which need to be considered!
+  if(NrFitHistos == 1) int ChosenHisto = 0;                                 //Choose 1 specific histogram!
+  //-------------------------------------------------------------------------------------//
+  
+  TFile* histoFile = new TFile( ("../CreatedTFFromDistributions_"+Directory+".root").c_str() ,"READ");
+  std::cout << endl << " //--------------------------------------------------------------------------------------------------------" << endl;
+  std::cout <<         " //-----------  Used ROOT file : " << histoFile->GetName() << endl;
+  std::cout <<         " //--------------------------------------------------------------------------------------------------------" << endl;
+
+  std::string Titles[12] ={"Light_DiffPtVsGenPt",      // Number 0
+			   "Light_DiffThetaVsGenPt",   // Number 4
+			   "Light_DiffPhiVsGenPt",     // Number 8
+			   "BJet_DiffPtVsGenPt",       // Number 1
+			   "BJet_DiffThetaVsGenPt",    // Number 5
+			   "BJet_DiffPhiVsGenPt",      // Number 9
+			   "El_DiffPtVsGenPt",         // Number 2
+			   "El_DiffThetaVsGenPt",      // Number 6
+			   "El_DiffPhiVsGenPt",        // Number 10
+			   "Mu_DiffInvPtVsGenInvPt",   // Number 3
+			   "Mu_DiffThetaVsGenInvPt",   // Number 7
+			   "Mu_DiffPhiVsGenInvPt"};    // Number 11
+  string HistoAxisName[5] ={"Difference (gen-reco) of transverse E","Difference (gen-reco) of inverse transverse p ",
+			    "Difference (gen-reco) of transverse p","Difference (gen-reco) of #phi","Difference (gen-reco) of #theta"};
+  string ChiXAxisName[3] ={"Transverse energy of generator parton (GeV)","Inverse transverse momentum of generator parton (1/GeV)","Transverse momentum of generator parton (GeV)"};
+  string HistoTitle[4] = {"light jets","b-jets","muon","electron"};
+  
+  if(GetMkdirOutput == true){
+    //--------------------------------------------//
+    //  First time: Create needed directories !!  //    
+    //--------------------------------------------//
+    std::cout << endl << " *** Directories which should be created ! *** " << endl << endl;
+    std::cout << " mkdir " << Directory << " ";
+    for(int ii = 0; ii < NrFitHistos; ii++)
+      std::cout << Directory << "/" << Titles[ii] << " ";
+    std::cout << " " << endl;
+    //--------------------------------------------//
+    std::cout << endl << " --> If histograms need to be created, change value of boolean 'GetMkdirOutput' to false !! " << endl;
+  }
+  else{
+    
+    //////////////////////////////////
+    // Save the fitting histograms! //
+    //////////////////////////////////
+    TCanvas* twoDCanvas = new TCanvas("fitCanvas","2-dimensional distributions used as fitting input");
+    twoDCanvas->Divide(3,4);
+    TH1D* twoDHisto;
+   
+    for(int iHisto = 0; iHisto < NrFitHistos; iHisto++){    //Loop over each of the fit histograms
+      if(NrFitHistos == 1) iHisto = ChosenHisto;
+     
+      string Title = Titles[iHisto];
+      string histoName, HistoName, histoTitle, histoAxisName, chiXAxisName;
+      TH1D* ProjHisto;
+      
+      //Set the correct X-axis name for the chi2 distribution
+      if(Title.find("GenEt") <= Title.size())         chiXAxisName = ChiXAxisName[0];   
+      else if(Title.find("GenInvPt") <= Title.size()) chiXAxisName = ChiXAxisName[1];
+      else if(Title.find("GenPt") <= Title.size())    chiXAxisName = ChiXAxisName[2];
+	  
+      //Set the title for the correct particle
+      if(Title.find("Light_") == 0)      histoTitle = HistoTitle[0];         
+      else if(Title.find("BJet_") == 0)  histoTitle = HistoTitle[1];
+      else if(Title.find("Mu_") == 0)    histoTitle = HistoTitle[2];
+      else if(Title.find("El_") == 0)    histoTitle = HistoTitle[3];
+      
+      //Set the correct axis variable
+      if(Title.find("DiffEt") <= Title.size())         histoAxisName = HistoAxisName[0];
+      else if(Title.find("DiffInvPt") <= Title.size()) histoAxisName = HistoAxisName[1];
+      else if(Title.find("DiffPt") <= Title.size())    histoAxisName = HistoAxisName[2];
+      else if(Title.find("DiffPhi") <= Title.size())   histoAxisName = HistoAxisName[3];
+      else if(Title.find("DiffTheta") <= Title.size()) histoAxisName = HistoAxisName[4];      
+      std::cout << " --- Found strings : (chiXAxis, histoTitle & histoAxisName) = " << chiXAxisName << ", " << histoTitle << " & " << histoAxisName << std::endl;
+ 
+      ///////////////////////////////
+      //  Draw fitting histograms  //
+      ///////////////////////////////
+      twoDHisto = (TH1D*) histoFile->Get( ("2D_histograms_graphs/"+Title).c_str() );
+      twoDHisto->GetXaxis()->SetTitle( chiXAxisName.c_str() );
+      twoDHisto->GetYaxis()->SetTitle( histoAxisName.c_str() );
+      twoDHisto->SetTitle( ("Color plot for "+histoTitle).c_str() );
+      twoDHisto->GetXaxis()->SetTitleOffset(0.85);
+      twoDHisto->GetYaxis()->SetTitleOffset(0.75);
+      twoDCanvas->cd(iHisto+1);
+      twoDHisto->Draw("colz");
+
+      TCanvas* indiv2DCanvas =  new TCanvas("indiv2DCanvas","title");
+      indiv2DCanvas->cd();
+      twoDHisto->Draw("colz");
+      indiv2DCanvas->SaveAs( (Directory+"/"+Title+".png").c_str() );
+      indiv2DCanvas->SaveAs( (Directory+"/"+Title+".pdf").c_str() );
+      delete indiv2DCanvas;
+       
+      ///////////////////////////////////////
+      //  Save the ProjectionY histograms  //
+      ///////////////////////////////////////
+      TCanvas* projCanvas = new TCanvas("projCanvas","Fit results of projectionY for each bin");
+      projCanvas->Divide(3,4);
+      
+      for(int iBin = 0; iBin <= NrBins; iBin++){
+	//Set name for .root file and for saving the histograms!
+	if(iBin < NrBins){
+	  histoName = ("sliceYbin"+HistoBin[iBin]).c_str();          
+	  HistoName = ("ProjectionY_Bin"+HistoBin[iBin]).c_str();
+	}
+	else{
+	  histoName = "chi2";
+	  HistoName = "Chi2Distribution";
+	}
+	projHisto = (TH1D*) histoFile->Get( (Title+"/"+Title+"_"+histoName).c_str() );
+	
+	//Set the Marker to a small dot
+	projHisto->SetMarkerStyle(8); //Only use a small dot!
+	projHisto->SetMarkerSize(0.3);
+	projHisto->GetXaxis()->SetTitleOffset(0.85);
+	projHisto->GetYaxis()->SetTitleOffset(0.80);
+
+	//Draw the histograms in the different pads of the created canvas and set Title & axis label!
+	projCanvas->cd(iBin+1);
+	if(iBin < NrBins)
+	  projHisto->Draw("e");
+	else{
+	  projCanvas_12->SetLogy();          //Last canvas (chi2) should be in logY 
+	  projHisto->Draw();                 //No error bars for the chi2 histogram!
+	}	
+	
+	//Save each histogram separately as well!
+	TCanvas* projIndivCanvas = new TCanvas("projIndivCanvas","title");;
+	projIndivCanvas->cd();
+	if(iBin < NrBins){
+	  projHisto->SetTitle( ("Fit result for "+histoTitle+" (bin "+HistoBin[iBin]+")").c_str() );
+	  projHisto->GetXaxis()->SetTitle( histoAxisName.c_str() );	  
+	}
+	else{
+	  projHisto->SetTitle(  (histoAxisName+": Fit result").c_str());     
+	  projHisto->GetYaxis()->SetTitle("#chi^{2} for double Gaussian fit");
+	  projIndivCanvas->SetLogy();
+	  projHisto->GetXaxis()->SetTitle(chiXAxisName.c_str() );	       
+	}
+	projHisto->Draw();	
+	projIndivCanvas->SaveAs( (Directory+"/"+Title+"/"+HistoName+".png").c_str() );    //Individual histograms!
+	projIndivCanvas->SaveAs( (Directory+"/"+Title+"/"+HistoName+".pdf").c_str() );
+	delete projIndivCanvas;
+	
+      }//End of loop over NrBins
+
+      projCanvas->SaveAs( (Directory+"/"+Title+"/Overview_FitDistributions.png").c_str() ); //Histograms together in one canvas!
+      projCanvas->SaveAs( (Directory+"/"+Title+"/Overview_FitDistributions.pdf").c_str() );      
+      delete projCanvas;
+      delete projHisto;      
+
+      ////////////////////////////////////////////
+      //  Save the fit parameter distributions  //
+      ////////////////////////////////////////////
+      TCanvas* paramCanvas = new TCanvas("paramCanvas","Fit results for the different parameters");
+      paramCanvas->Divide(2,3);
+
+      TH1D* paramHisto;
+      for(int iPar = 0; iPar < NrParams; iPar++){
+	paramHisto = (TH1D*) histoFile->Get( (Title+"/"+Title+"_"+Param[iPar]+"_PointsAndFit").c_str() );
+	paramHisto->SetTitle( ("Fitted value of parameter "+Param[iPar]).c_str());
+	paramHisto->GetXaxis()->SetTitle( ChiXAxisName.c_str() );
+	paramHisto->GetXaxis()->SetTitleOffset(0.85);
+	paramHisto->SetMarkerStyle(8); //Only use a small dot!
+	paramHisto->SetMarkerSize(0.3);
+	
+	//Draw the histograms in the different pads of the created canvas!
+	paramCanvas->cd(iPar+1);
+	paramHisto->Draw("e");
+
+	//Also draw the histograms individually
+	TCanvas* paramIndivCanvas = new TCanvas("paramIndivCanvas","title");
+	paramIndivCanvas->cd();
+	paramHisto->Draw("e");
+	string ParamHistoName = ("FitParameter_"+Param[iPar]).c_str();
+	paramIndivCanvas->SaveAs( (Directory+"/"+Title+"/"+ParamHistoName+".png").c_str() ); //Parameter histograms individual!
+	paramIndivCanvas->SaveAs( (Directory+"/"+Title+"/"+ParamHistoName+".pdf").c_str() );
+	delete paramIndivCanvas;
+	
+      }//End of loop over NrParams
+
+      paramCanvas->SaveAs( (Directory+"/"+Title+"/Overview_FitParameters.png").c_str() ); //Parameter histograms together in one canvs!
+      paramCanvas->SaveAs( (Directory+"/"+Title+"/Overview_FitParameters.pdf").c_str() );
+      delete paramCanvas;
+      delete paramHisto;
+
+    }//End of loop over NrFitHistos
+    
+    twoDCanvas->SaveAs( (Directory+"/"+"ColorPlots.png").c_str() );
+    twoDCanvas->SaveAs( (Directory+"/"+"ColorPlots.pdf").c_str() ); 
+    delete twoDCanvas;
+    delete twoDHisto;    
+  }
+}
