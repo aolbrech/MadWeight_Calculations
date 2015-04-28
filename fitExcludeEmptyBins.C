2e7f0b3872fdbf30dc60854da7e0403072879e1e
Annik <annik.olbrechts@cern.ch>
2015-04-28 15:11:49 +0200

    Changed file to be compatible for both with and without cut! New histogram in limited range is created for fitting

--- fitExcludeEmptyBins.C
+++ fitExcludeEmptyBins.C
@@ -3,16 +3,8 @@
 #include "TList.h"
 #include "TFile.h"
 #include "TCanvas.h"
-
-Bool_t reject;
-Double_t fpol(Double_t *x, Double_t *par)
-{
-  if (reject && ( x[0] != 153 && x[0] != 163 && x[0] != 171 && x[0] != 172 && x[0] != 173 && x[0] != 174 && x[0] != 175 && x[0] != 183 && x[0] != 193) ){ 
-    TF1::RejectPoint();
-    return 0;
-  }
-  return par[0] + par[1]*x[0] + par[2]*x[0]*x[0];
-}
+#include "TGraph.h"
+#include "TStyle.h"
 
 Double_t Minimum(TH1F* histo)
 {
@@ -25,59 +17,69 @@ Double_t Minimum(TH1F* histo)
 }
 
 void fitExcludeEmptyBins() {
+
   //Get the histogram from the ROOT file   --> Name gets changed by pythonScript!!
-  TFile* InputFile = new TFile(" Events_ContainingMTopScansForRecLevelEvts/TopMass_Gen_ManyEvts/Likelihood_TopMass_Gen_UsingWeights.root ","r");
-  TFile* OutputFile = new TFile(" Events_ContainingMTopScansForRecLevelEvts/TopMass_Gen_ManyEvts/LimitedFitResult_Gen.root ","RECREATE");
+  TFile* InputFile = new TFile(" Events/RVR_RecoUnmatched_SingleGausTF_10000Evts_SmallerSteps_SecondRun/FitDeviation_UnmatchedReco_RVR_10000Evts.root ","r");
+  TFile* OutputFile = new TFile(" Events/RVR_RecoUnmatched_SingleGausTF_10000Evts_SmallerSteps_SecondRun/LimitedFitResult_UnmatchedReco_RVR_ScdDerBothCutApplied.root ","RECREATE");
+  
+  bool GenLevel = false; 
+  
+  TH1F* LL    = (TH1F*) InputFile->Get("LikelihoodAfterCuts/SignSecondDerivative/LLPosScdDerBoth"); 
+  TH1F* LLXS  = (TH1F*) InputFile->Get("LikelihoodAfterCuts/SignSecondDerivative/LLXSPosScdDerBoth"); 
+  TH1F* LLAcc = (TH1F*) InputFile->Get("LikelihoodAfterCuts/SignSecondDerivative/LLAccPosScdDerBoth"); 
 
-  bool GenLevel = true; 
+  //-- Store the zoomed information in a new histogram which will be used for fitting! --//
+  int nBins = 5;         //Initialize nrBins 
+  float fitStart = -0.125;  //Initialize fitStart 
+  float fitEnd = 0.125;    //Initialize fitEnd 
+  TH1F* LLFit    = new TH1F("LLFit",   "LL histogram for fit",   nBins,fitStart,fitEnd);
+  TH1F* LLXSFit  = new TH1F("LLXSFit", "LLXS histogram for fit", nBins,fitStart,fitEnd);
+  TH1F* LLAccFit = new TH1F("LLAccFit","LLAcc histogram for fit",nBins,fitStart,fitEnd);
 
-  TH1F* LL    = (TH1F*) InputFile->Get("LL");
-  TH1F* LLXS  = (TH1F*) InputFile->Get("LL_XS");
-  TH1F* LLAcc = (TH1F*) InputFile->Get("LL_Acc");
-
-  TF1 *func = new TF1("func",fpol,153,193,3); 
-  func->SetName("fit_LL");
-  reject = kTRUE;
-  LL->Fit(func);
-  reject = kFALSE;
-  TCanvas* LLCanvas = new TCanvas("LLCanvas","Distribution of -ln(likelihood) (no normalisation)");
-  LLCanvas->cd();
-  LL->SetMinimum(Minimum(LL)*0.999);
-  func->Draw();
-  func->SetTitle(LL->GetTitle());
-  LL->Draw("SAMEP");
-  LLCanvas->Write();
-  func->Write();
-
-  TF1 *funcXS = new TF1("funcXS",fpol,153,193,3); 
-  funcXS->SetName("fit_LLXS");
-  reject = kTRUE;
-  LLXS->Fit(funcXS);
-  reject = kFALSE;
-  TCanvas* LLXSCanvas = new TCanvas("LLXSCanvas","Distribution of -ln(likelihood) (XS normalisation)");
-  LLXSCanvas->cd();
-  LLXS->SetMinimum(Minimum(LLXS)*0.99);
-  funcXS->Draw();
-  funcXS->SetTitle(LLXS->GetTitle());
-  LLXS->Draw("PSAME");
-  LLXSCanvas->Write();
-  funcXS->Write();
-
-  if(GenLevel == false){
-  TF1 *funcAcc = new TF1("funcAcc",fpol,153,193,3); 
-    funcAcc->SetName("fit_LLAcc");
-    reject = kTRUE;
-    LLAcc->Fit(funcAcc);
-    reject = kFALSE;
-    TCanvas* LLAccCanvas = new TCanvas("LLAccCanvas","Distribution of -ln(likelihood) (Acc normalisation)");
-    LLAccCanvas->cd();
-    LLAcc->SetMinimum(Minimum(LLAcc)*0.99);
-    funcAcc->Draw();
-    funcAcc->SetTitle(LLAcc->GetTitle());
-    LLAcc->Draw("PSAME");
-    LLAccCanvas->Write();
-    funcAcc->Write();
+  float firstBin = (fitEnd-fitStart)/(nBins*2)+fitStart;
+  for(int ii = 0; ii < nBins; ii++){
+    LLFit->SetBinContent(ii+1,LL->GetBinContent(LL->FindBin(firstBin+ii*((fitEnd-fitStart)/nBins)))); 
+    LLXSFit->SetBinContent(ii+1,LLXS->GetBinContent(LLXS->FindBin(firstBin+ii*((fitEnd-fitStart)/nBins)))); 
+    if(GenLevel == false) LLAccFit->SetBinContent(ii+1,LLAcc->GetBinContent(LLAcc->FindBin(firstBin+ii*((fitEnd-fitStart)/nBins)))); 
   }
+  LL->SetMarkerStyle(20);   LL->SetMarkerColor(1);     
+  LLXS->SetMarkerStyle(21); LLXS->SetMarkerColor(1); 
+  if(GenLevel == false){ LLAcc->SetMarkerStyle(22); LLAcc->SetMarkerColor(1);} 
+  LLFit->Write(); LLXSFit->Write();
 
+  //-- Fit the three different histograms! --//
+  TF1 *func = new TF1("func","pol2",fitStart,fitEnd);     func->SetName("fit_LL");     LLFit->Fit(func);
+  TF1 *funcXS = new TF1("funcXS","pol2",fitStart,fitEnd); funcXS->SetName("fit_LLXS"); LLXSFit->Fit(funcXS);
+  if(GenLevel == false){TF1 *funcAcc = new TF1("funcAcc","pol2",fitStart,fitEnd); funcAcc->SetName("fit_LLAcc"); LLAccFit->Fit(funcAcc);}
+
+  //-- Make graph of fit function! --//
+  Double_t x[100], y[100], yXS[100], yAcc[100];
+  for(int i = 0; i <LL->GetNbinsX(); i++){
+    x[i] = LL->GetBinCenter(i+1);
+    y[i]    = func->GetParameter(0)   +func->GetParameter(1)*x[i]+   func->GetParameter(2)*x[i]*x[i];
+    yXS[i]  = funcXS->GetParameter(0) +funcXS->GetParameter(1)*x[i]+ funcXS->GetParameter(2)*x[i]*x[i];
+    if(GenLevel == false) yAcc[i] = funcAcc->GetParameter(0)+funcAcc->GetParameter(1)*x[i]+funcAcc->GetParameter(2)*x[i]*x[i];
+  }
+  gr    = new TGraph(LL->GetNbinsX(),x,y);       gr->SetLineColor(2);    gr->SetLineStyle(2);
+  grXS  = new TGraph(LLXS->GetNbinsX(),x,yXS);   grXS->SetLineColor(2);  grXS->SetLineStyle(2);
+  if(GenLevel == false){grAcc = new TGraph(LLAcc->GetNbinsX(),x,yAcc); grAcc->SetLineColor(2); grAcc->SetLineStyle(2);}
+
+  //-- Draw all the histograms in a single canvas! --//
+  TCanvas* LLCanvas = new TCanvas("LLCanvas","Distribution of -ln(likelihood) (no normalisation)"); LLCanvas->cd();
+  LL->SetMinimum(Minimum(LL)*0.999); 
+  LL->Draw("P"); gr->Draw("C"); func->Draw("same"); LLCanvas->Write();
+  func->SetTitle(LL->GetTitle()); func->Write();
+  
+  TCanvas* LLXSCanvas = new TCanvas("LLXSCanvas","Distribution of -ln(likelihood) (XS normalisation)"); LLXSCanvas->cd();
+  LLXS->SetMinimum(Minimum(LLXS)*0.99); 
+  LLXS->Draw("P"); grXS->Draw("C"); funcXS->Draw("same"); LLXSCanvas->Write();
+  funcXS->SetTitle(LLXS->GetTitle()); funcXS->Write();
+  
+  if(GenLevel == false){
+    TCanvas* LLAccCanvas = new TCanvas("LLAccCanvas","Distribution of -ln(likelihood) (Acc normalisation)"); LLAccCanvas->cd();
+    LLAcc->SetMinimum(Minimum(LLAcc)*0.99); 
+    LLAcc->Draw("P"); grAcc->Draw("C"); funcAcc->Draw("same"); LLAccCanvas->Write();
+    funcAcc->SetTitle(LLAcc->GetTitle()); funcAcc->Write();
+  }
   OutputFile->Close();
 }
