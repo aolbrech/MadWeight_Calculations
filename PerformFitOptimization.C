0662976e54586abbf488533b9a198cfdbb5eec30
Annik <annik.olbrechts@cern.ch>
2015-05-13 18:04:37 +0200

    Added PaintOverflow function + Histograms for chi-squared cut

--- PerformFitOptimization.C
+++ PerformFitOptimization.C
@@ -15,17 +15,47 @@
 #include <cstring>
 #include <cstdlib>
 
-TFile *inFile = new TFile("FitDistributions_10000RecoEvts_ManySteps.root","READ");       //Should be filled by the python script!
-//TDirectory *dir_FstFit = inFile->GetDirectory("FirstPolynomialFit"), *dir_FstFitXS = inFile->GetDirectory("FirstPolynomialFit_XS"), *dir_FstFitAcc = inFile->GetDirectory("FirstPolynomialFit_Acc");
-//TDirectory *dir_ScdFit = inFile->GetDirectory("SecondPolynomialFit"),*dir_ScdFitXS = inFile->GetDirectory("SecondPolynomialFit_XS"),*dir_ScdFitAcc = inFile->GetDirectory("SecondPolynomialFit_Acc");
-//TDirectory *dir_OrigLL = inFile->GetDirectory("OriginalLL"),         *dir_OrigLLXS = inFile->GetDirectory("OriginalLL_XS"),         *dir_OrigLLAcc = inFile->GetDirectory("OriginalLL_Acc");
-
+//TFile *inFile = new TFile("FitDistributions_10000RecoEvts_ManySteps.root","READ");       //Should be filled by the python script!
+TFile *inFile = new TFile("FitDistributions.root","READ");       //Should be filled by the python script!
 TFile *outputFile = new TFile("FitOptimizations.root","RECREATE"); //Should be filled by the python script!
 
 int NrEvts = 10; 
 int xBin = 21;        //Copy this from the python file!
 float xLow = -0.105;  //Copy this from the python file!
 float xHigh = 0.105;  //Copy this from the python file!
+
+void PaintOverflow(TH1F *h, TFile *FileToWrite, std::string dirName){     // This function draws the histogram h with an extra bin for overflows
+  Int_t nx    = h->GetNbinsX()+1;
+  Double_t x1 = h->GetBinLowEdge(1), bw = h->GetBinWidth(nx), x2 = h->GetBinLowEdge(nx)+bw;
+
+  //Define a temporary histogram having an extra bin for overflows
+  char newTitle[100], newName[100];
+  strcpy(newTitle,h->GetTitle()); strcat(newTitle," (under- and overflow added)" );
+  strcpy(newName,h->GetName());  strcat(newName, "_Flow");
+  TH1F *h_tmp = new TH1F(newName, newTitle, nx, x1, x2);
+
+  // Fill the new histogram including the extra bin for overflows
+  for (Int_t i=1; i<=nx; i++)
+    h_tmp->Fill(h_tmp->GetBinCenter(i), h->GetBinContent(i));
+  // Fill the underflows
+  h_tmp->Fill(x1-1, h->GetBinContent(0));
+
+  // Restore the number of entries
+  h_tmp->SetEntries(h->GetEntries());
+
+  //Set the correct path to save the file
+  const char* path = 0;
+  if(dirName != ""){
+    TDirectory *dir = FileToWrite->GetDirectory(dirName.c_str());
+    if (!dir)
+      dir = FileToWrite->mkdir(dirName.c_str());
+    path = dir->GetName();
+  }
+
+  FileToWrite->cd(path);
+  h_tmp->Write();
+  FileToWrite->cd();  //Reset to general directory! 
+}                  
 
 void getIndividualDirObjects(TDirectory *dir){
   outputFile->cd();
@@ -41,7 +71,8 @@ void getIndividualDirObjects(TDirectory *dir){
     //Convert name of directory (const char*) to a string:
     std::string dirName(dir->GetName(), 0, 100);
 
-    TH1F *h_LLSum = 0, *h_FitSum = 0;
+    TH1F *h_LLSum = 0;
+    TH1F *h_FitSum = 0, *h_ChiSq = 0, *h_FitSumSmallChiSq = 0, *h_SlopeFit = 0, *h_FitSumSmallChiSqPosSlope;
     //Identify which directory is being studied --> Containing TF1 or TH1F & type of normalisation!
     std::string NormType = "", FitNr = "", NrUsedPoints = "";
     bool lookAtFits = false;
@@ -57,10 +88,16 @@ void getIndividualDirObjects(TDirectory *dir){
     else{
       lookAtFits = true;
       h_FitSum = new TH1F((FitNr+""+NormType+"_Summed").c_str(), ("Distribution of "+FitNr+" after summing over "+NrUsedPoints+" points").c_str(), xBin, xLow, xHigh);
+      h_SlopeFit = new TH1F((FitNr+""+NormType+"_SlopeVar").c_str(), ("Distribution of slope of "+FitNr).c_str(), 200, -5.0, 5.0);
+      h_ChiSq = new TH1F( ("ChiSq"+NormType+"_"+FitNr).c_str(), ("ChiSquared distribution of "+FitNr+" (norm = "+NormType+")").c_str(),200, 0, 0.005);
+      h_FitSumSmallChiSq = new TH1F((FitNr+""+NormType+"_SmallChiSq").c_str(), ("Distribution of "+FitNr+"after summing over "+NrUsedPoints+" points (ChiSq-cut applied)").c_str(),xBin,xLow,xHigh);
+      h_FitSumSmallChiSqPosSlope = new TH1F((FitNr+""+NormType+"_SmallChiSqPosSlope").c_str(), ("Distribution of "+FitNr+"after summing over "+NrUsedPoints+" points (ChiSq-cut applied, positive slope required)").c_str(),xBin,xLow,xHigh);
     }
-    
+   
+    //Initialize counters:
+    int allEvts = 0, evtsSmallChiSq = 0, evtsSmallChiSqPosSlope = 0;
+ 
     std::string fitName = "";
-    double fitXMin = 0, fitXMax = 0;
     while ((object_dir = next())) {         //Keep it general to TObject because both TF1 and TH1F are stored in this ROOT file!
       if(lookAtFits == false){  //Looking at TH1F objects!
         TH1F *h_iLL = (TH1F*) dir->Get(object_dir->GetName());
@@ -69,11 +106,29 @@ void getIndividualDirObjects(TDirectory *dir){
       else if(lookAtFits == true){ //Looking at TF1 objects!
         TF1 *fitFunc = (TF1*) dir->Get(object_dir->GetName());
         h_FitSum->Add(fitFunc);
+        h_SlopeFit->Fill(fitFunc->GetParameter(2));
+        allEvts++;
+        h_ChiSq->Fill(fitFunc->GetChisquare());
+        if(fitFunc->GetChisquare() < 0.0002){
+          h_FitSumSmallChiSq->Add(fitFunc);
+          evtsSmallChiSq++;
+          if(fitFunc->GetParameter(2) > 0){
+            h_FitSumSmallChiSqPosSlope->Add(fitFunc);
+            evtsSmallChiSqPosSlope++;
+          }
+        }
       }
     }
 
-    if(lookAtFits == true)
+    if(lookAtFits == true){
       h_FitSum->Write();
+      PaintOverflow(h_SlopeFit,outputFile,"");
+      PaintOverflow(h_ChiSq,outputFile,"ChiSq");
+      h_FitSumSmallChiSq->Write();
+      std::cout << " Histogram " << h_FitSumSmallChiSq->GetName() << " has " << evtsSmallChiSq << "/" << allEvts << " events selected !" << std::endl;
+      h_FitSumSmallChiSqPosSlope->Write();
+      std::cout << " Histogram " << h_FitSumSmallChiSqPosSlope->GetName() << " has " << evtsSmallChiSqPosSlope << "/" << allEvts << " events selected !" << std::endl;
+    }
     else 
       h_LLSum->Write();
   }
