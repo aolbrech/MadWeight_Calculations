b87718325a379ad07b77a8894a00a47431906df9
Annik <annik.olbrechts@cern.ch>
2015-07-09 16:37:43 +0200

    Adding one of the original distributions where only events with a positive slope (and hence minimum-like shape) are selected

--- PerformFitOptimization.C
+++ PerformFitOptimization.C
@@ -17,17 +17,17 @@
 /////////////////////////////////////////////////////////////
 // Specify whether the stacked canvasses have to be stored //
 bool storeSplittedCanvas = false; 
-std::string SplittedDir = "Events_RgRScan/RgR_RECO_SingleGausTF_10000Evts_FullRange/SplittedCanvasses"; 
+std::string SplittedDir = "Events_RgRScan/RgR_MGSamplePos005_SingleGausTF_10000Evts_NarrowRange_CutsAppliedAlsoOnMET/SplittedCanvasses"; 
 /////////////////////////////////////////////////////////////
 
-TFile *inFile = new TFile("Events_RgRScan/RgR_RECO_SingleGausTF_10000Evts_FullRange/FitDistributions_RgR_RECO_SingleGausTF_10000Evts_FullRange_8602Evts.root","READ"); 
-TFile *outputFile = new TFile("Events_RgRScan/RgR_RECO_SingleGausTF_10000Evts_FullRange/FitOptimizations_RgR_RECO_SingleGausTF_10000Evts_FullRange_8602Evts.root","RECREATE"); 
+TFile *inFile = new TFile("Events_RgRScan/RgR_MGSamplePos005_SingleGausTF_10000Evts_NarrowRange_CutsAppliedAlsoOnMET/FitDistributions_RgR_MGSamplePos005_SingleGausTF_10000Evts_NarrowRange_CutsAppliedAlsoOnMET_CosThetaReweightingApplied_10000Evts.root","READ"); 
+TFile *outputFile = new TFile("Events_RgRScan/RgR_MGSamplePos005_SingleGausTF_10000Evts_NarrowRange_CutsAppliedAlsoOnMET/FitOptimizations_RgR_MGSamplePos005_SingleGausTF_10000Evts_NarrowRange_CutsAppliedAlsoOnMET_CosThetaReweightingApplied_10000Evts.root","RECREATE"); 
 
 int NrEvts = 10; 
-const int xBin = 41; 
+const int xBin = 9; 
 const int xFitBin = xBin*10; 
-float xLow = -0.5125; 
-float xHigh = 0.5125; 
+float xLow = -0.225; 
+float xHigh = 0.225; 
 
 void PaintOverflow(TH1F *h, TFile *FileToWrite, std::string dirName){     // This function draws the histogram h with an extra bin for overflows
   Int_t nx    = h->GetNbinsX()+1;
@@ -82,15 +82,15 @@ void getIndividualDirObjects(TDirectory *dir){
     std::string dirName(dir->GetName(), 0, 100);
 
     //Specify which chi-sq cut values should be considered:
-    float ChiSqCutsFstPol[4] = {0.0002, 0.001, 0.0005, 0.005}; 
-    float ChiSqCutsScdPol[4] = {0.0002, 0.001, 0.0005, 0.005}; 
+    float ChiSqCutsFstPol[4] = {0.0005, 0.00008, 0.00005, 0.00001}; 
+    float ChiSqCutsScdPol[4] = {0.0002, 0.00005, 0.00001, 0.000005}; 
     const int NrChiSqCutsFstPol = sizeof(ChiSqCutsFstPol)/sizeof(float);
     const int NrChiSqCutsScdPol = sizeof(ChiSqCutsScdPol)/sizeof(float);
     const int NrChiSqCuts = NrChiSqCutsFstPol;
     //if(NrChiSqCutsFstPol > NrChiSqCutsScdPol) NrChiSqCuts = (const int) NrChiSqCutsScdPol;  //Take the lowest number as the number of cuts!
 
     TH1F *h_LLSum = 0;
-    TH1F *h_FitSum = 0, *h_LLSumAll = 0;
+    TH1F *h_FitSum = 0, *h_LLSumAll = 0, *h_FitSumPosSlope = 0;
     TH1F *h_ChiSq = 0, *h_FitSumSmallChiSq[NrChiSqCuts] = {0}, *h_LLSumSmallChiSq[NrChiSqCuts] = {0}, *h_SlopeFit = 0, *h_FitSumSmallChiSqPosSlope[NrChiSqCuts] = {0};
     //Identify which directory is being studied --> Containing TF1 or TH1F & type of normalisation!
     std::string NormType = "", NormTypeDir = "", FitNr = "", NrUsedPoints = "";
@@ -116,12 +116,13 @@ void getIndividualDirObjects(TDirectory *dir){
       h_SlopeFit = new TH1F(("Slope_"+FitNr+""+NormType).c_str(), "title",200,-5.0, 5.0  );
       h_ChiSq = new TH1F( ("ChiSq"+NormType+"_"+FitNr).c_str(),   "title",200,0,    0.0005);
       h_LLSumAll = new TH1F( ("Hist_"+FitNr+""+NormType+"_All").c_str(),"title",xBin,xLow,xHigh);
+      h_FitSumPosSlope = new TH1F((FitNr+""+NormType+"_PosSlopeSummed").c_str(),"title",xFitBin,xLow,xHigh);
     }
    
     //Initialize counters and histograms:
     int allEvts = 0;
     int evtsSmallChiSq[NrChiSqCuts] = {0}, evtsSmallChiSqPosSlope[NrChiSqCuts] = {0};
-    double VarBinValue[xFitBin] = {0.};
+    double VarBinValue[xFitBin] = {0.}, VarBinValuePosSlope[xFitBin] = {0.};
     double VarBinValueSmallChiSq[NrChiSqCuts][xFitBin] = {{0.}};
     //double VarBinValueSmallChiSqPosSlope[NrChiSqCuts][xFitBin] = {{0.}};
     for(int ii = 0; ii < NrChiSqCuts; ii++){
@@ -140,6 +141,7 @@ void getIndividualDirObjects(TDirectory *dir){
       else if(lookAtFits == true){ //Looking at TF1 objects!
         TF1 *fitFunc = (TF1*) dir->Get(object_dir->GetName());
         h_FitSum->Add(fitFunc);
+        if(fitFunc->GetParameter(2) > 0) h_FitSumPosSlope->Add(fitFunc);
         h_SlopeFit->Fill(fitFunc->GetParameter(2));
         allEvts++;
         h_ChiSq->Fill(fitFunc->GetChisquare());
@@ -151,11 +153,11 @@ void getIndividualDirObjects(TDirectory *dir){
         TH1F *h_OrigLL = (TH1F*) inFile->GetDirectory(("OriginalLL"+NormTypeDir).c_str())->Get(("LnLik"+NormType+"_"+EvtNrFit).c_str());
         h_LLSumAll->Add(h_OrigLL);                      //Not possible to access the TH1F object at this point ??
 
-
         for(int iBin = 0; iBin < xFitBin; iBin++){
           double VarValue = xLow+(xHigh-xLow)/(2*xFitBin)+((xHigh-xLow)/xFitBin)*iBin;
           for(int iPar = 0; iPar < fitFunc->GetNpar(); iPar++)
             VarBinValue[iBin] += fitFunc->GetParameter(iPar)*pow(VarValue,iPar);
+            if(fitFunc->GetParameter(2) > 0) VarBinValuePosSlope += fitFunc->GetParameter(iPar)*pow(VarValue,iPar);
         }
 
         //Apply the chi-sq cuts for each of the values requested:
@@ -169,15 +171,15 @@ void getIndividualDirObjects(TDirectory *dir){
                 VarBinValueSmallChiSq[iChiSq][iBin] += fitFunc->GetParameter(iPar)*pow(VarValue,iPar);
             }
 
-            //if(fitFunc->GetParameter(2) > 0){                               //Does this requirement make any sense when a pol4 fit is applied ???
-            //  h_FitSumSmallChiSqPosSlope[iChiSq]->Add(fitFunc);
-            //  evtsSmallChiSqPosSlope[iChiSq]++;
-            //  for(int iBin = 0; iBin < xFitBin; iBin++){
-            //    double VarValue = xLow+(xHigh-xLow)/(2*xFitBin)+((xHigh-xLow)/xFitBin)*iBin;
-            //    for(int iPar = 0; iPar < fitFunc->GetNpar(); iPar++)
-            //      VarBinValueSmallChiSqPosSlope[iChiSq][iBin] += fitFunc->GetParameter(iPar)*pow(VarValue,iPar);
-            //  }
-            //}
+            /*if(fitFunc->GetParameter(2) > 0){                               //Does this requirement make any sense when a pol4 fit is applied ???
+              h_FitSumSmallChiSqPosSlope[iChiSq]->Add(fitFunc);
+              evtsSmallChiSqPosSlope[iChiSq]++;
+              for(int iBin = 0; iBin < xFitBin; iBin++){
+                double VarValue = xLow+(xHigh-xLow)/(2*xFitBin)+((xHigh-xLow)/xFitBin)*iBin;
+                for(int iPar = 0; iPar < fitFunc->GetNpar(); iPar++)
+                  VarBinValueSmallChiSqPosSlope[iChiSq][iBin] += fitFunc->GetParameter(iPar)*pow(VarValue,iPar);
+              }
+            }*/
           }
         }
       }
@@ -203,6 +205,8 @@ void getIndividualDirObjects(TDirectory *dir){
           //std::cout << iBin+1 << ") Difference between bincontent and calculated value (FitSum) is : " << h_FitSum->GetBinContent(iBin+1) << " <--> " << VarBinValue[iBin] << std::endl;
           h_FitSum->SetBinContent(iBin+1, VarBinValue[iBin]);
         }
+        if(abs((h_FitSumPosSlope->GetBinContent(iBin+1)) - VarBinValuePosSlope[iBin]) > 1)
+          h_FitSumPosSlope->SetBinContent(iBin+1, VarBinValuePosSlope[iBin]);
       }
 
       for(int ii = 0; ii < NrChiSqCuts; ii++){
@@ -212,10 +216,10 @@ void getIndividualDirObjects(TDirectory *dir){
             h_FitSumSmallChiSq[ii]->SetBinContent(iBin+1, VarBinValueSmallChiSq[ii][iBin]);
           }
 
-          //if(abs((h_FitSumSmallChiSqPosSlope[ii]->GetBinContent(iBin+1)) - VarBinValueSmallChiSqPosSlope[ii][iBin]) > 1){  
+          /*if(abs((h_FitSumSmallChiSqPosSlope[ii]->GetBinContent(iBin+1)) - VarBinValueSmallChiSqPosSlope[ii][iBin]) > 1){  
             //cout << iBin+1 << " - " << ii << ") BinContent != VarBinValue (SmallChiSqPosSlope) : " << h_FitSumSmallChiSqPosSlope[ii]->GetBinContent(iBin+1) << " vs " << VarBinValueSmallChiSqPosSlope[ii][iBin] << endl;
-          //  h_FitSumSmallChiSqPosSlope[ii]->SetBinContent(iBin+1, VarBinValueSmallChiSqPosSlope[ii][iBin]);
-          //}
+            h_FitSumSmallChiSqPosSlope[ii]->SetBinContent(iBin+1, VarBinValueSmallChiSqPosSlope[ii][iBin]);
+          }*/
         }
 
         std::string sChiSqCut = "";
@@ -236,6 +240,7 @@ void getIndividualDirObjects(TDirectory *dir){
       //Now write away the histograms
       OrigDir->cd(); 
       h_FitSum->Write(); //h_LLSumAll->Write(); 
+      h_FitSumPosSlope->Write();
       outputFile->cd();
       PaintOverflow(h_SlopeFit,outputFile,"SlopeDistributions");
       PaintOverflow(h_ChiSq,outputFile,"ChiSqDistributions");
