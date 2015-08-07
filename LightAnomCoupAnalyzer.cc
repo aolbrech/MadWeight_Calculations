ba9c4b3beaf2da8f77c670383b832c4fd93c0bfb
Annik <annik.olbrechts@cern.ch>
2015-08-07 09:59:39 +0200

    Adding MSPlots and histograms (so needed to find correct way to add luminosity: hard-coded since EquivalentLumi option of dataset can differ from data one if only one sample has been recreated!)

--- LightAnomCoupAnalyzer.cc
+++ LightAnomCoupAnalyzer.cc
@@ -9,11 +9,14 @@
 #include "TDirectory.h"
 #include "TClonesArray.h"
 #include "TopTreeAnalysisBase/Content/interface/Dataset.h"
+#include "TopTreeAnalysisBase/Tools/interface/MultiSamplePlot.h"
+#include <sys/stat.h>  //Needed for mkdir option
 
 #include "TLorentzVector.h"
 
 #include "TROOT.h"
 #include "TH1F.h"
+#include "TH2F.h"
 #include "PersonalClasses/Style.C"                                                 //CHECK if this works!
 
 #include "AnomalousCouplings/PersonalClasses/interface/AnomCoupLight.h"
@@ -32,18 +35,43 @@ int main (int argc, char *argv[])
   //setTDRStyle(); 
   setMyStyle();
 
-  int verbose = 1;
+  //------------------------//
+  //  Verbosity for output  //
+  //  -->Used in bTagStudy  //
+  //------------------------//
+  int verbosity                 = 1;
+  //0 muet
+  //1 Main Info
+  //2 mcParticlesMatchin Info
+  //3 
+  //4 Info for each event
+  //5 Debug
 
+  //----------------------------//
+  // Input & output information //
+  //----------------------------//
   TFile* outputFile = new TFile("LightAnomCoup_Analysis.root","RECREATE");
 
   vector<string> inputFiles;
   vector<Dataset*> datasets;
   inputFiles.push_back("LightTree/AnomalousCouplingsLight_TTbarJets_SemiLept.root");
 
-  BTagStudy bTagStudy(verbose);
+  //-------------------------//
+  // Set analysis luminosity //
+  //-------------------------//
+  float Luminosity = 19646.840; //IsoMu24_eta2p1 trigger for semiMu case!  
+  std::cout << " Analysis luminosity is set to : " << Luminosity << std::endl;
+
+  //-----------------------//
+  // Load personal classes //
+  //-----------------------//
+  BTagStudy bTagStudy(verbosity);
+
   //-----------------------//
   // Initialize histograms //
   //-----------------------//
+  map<string,TH1F*> histo1D;
+  map<string,TH2F*> histo2D;  
 
   //-------------------//
   // Load all datasets //
@@ -58,7 +86,6 @@ int main (int argc, char *argv[])
     inConfigTree->GetEvent(0);
     Dataset* dataSet = (Dataset*) tc_dataset->At(0);
     //XSection = dataSet->Xsection();
-    //EqLumi = dataSet->EquivalentLumi();        
 
     int color = 0;
     if(dataSet->Name().find("QCD") == 0)             color = kYellow;
@@ -78,6 +105,18 @@ int main (int argc, char *argv[])
   //  Initialize MSPlots      //
   //  --> Needs dataset info  //
   //--------------------------//
+  map<string,MultiSamplePlot*> MSPlot;
+  string leptFlavs[2]={"_mu","_el"};
+  for(int ii = 0; ii < 2; ii++){
+    string leptFlav = leptFlavs[ii];
+ 
+    MSPlot["nSelectedJets_BeforeBTag"+leptFlav] = new MultiSamplePlot(datasets, "nSelectedJets_BeforeBTag"+leptFlav,14, -3.5, 10.5, "# selected jets");
+    MSPlot["nBTaggedJets_BeforeBTag"+leptFlav]  = new MultiSamplePlot(datasets, "nBTaggedJets_BeforeBTag"+leptFlav, 14, -3.5, 10.5, "# b-tagged jets");
+    MSPlot["nLightJets_BeforeBTag"+leptFlav]    = new MultiSamplePlot(datasets, "nLightJets_BeforeBTag"+leptFlav,   14, -3.5, 10.5, "# light jets");
+    MSPlot["nSelectedJets_AfterBTag"+leptFlav]  = new MultiSamplePlot(datasets, "nSelectedJets_AfterBTag"+leptFlav, 14, -3.5, 10.5, "# selected jets");
+    MSPlot["nBTaggedJets_AfterBTag"+leptFlav]   = new MultiSamplePlot(datasets, "nBTaggedJets_AfterBTag"+leptFlav,  14, -3.5, 10.5, "# b-tagged jets");
+    MSPlot["nLightJets_AfterBTag"+leptFlav]     = new MultiSamplePlot(datasets, "nLightJets_AfterBTag"+leptFlav,    14, -3.5, 10.5, "# light jets");
+  }
 
   //--------------------------------//
   // Lumi reweighting and lepton SF //
@@ -117,6 +156,9 @@ int main (int argc, char *argv[])
       TLorentzVector selectedLepton = light->selectedLepton();
       TLorentzVector MET = light->met();
       int decayChannel = light->decayChannel();  //0 = semiMu and 1 = semiEl
+      std::string leptChannel;
+      if(decayChannel == 0) leptChannel = "_mu";
+      else if(decayChannel == 1) leptChannel = "_el";
       vector<int> correctJetCombi = light->correctJetCombi();    //0 = LeptB, 1 = HadrB, 2 = Quark1 & 3 = Quark2
 
       //ooooooooOOOOOOOOOOOOooooooooooooOOOOOOOOOOOOooooooooooooOOOOO
@@ -125,15 +167,84 @@ int main (int argc, char *argv[])
       //ooOOooOOoo      Start of actual analysis           ooOOooOOoo
       //ooooooooOOOOOOOOOOOOooooooooooooOOOOOOOOOOOOooooooooooooOOOOO
       bTagStudy.CalculateJets(selectedJets, bTagCSV, correctJetCombi, selectedLepton);
-      float bTagLeptIndex = bTagStudy.getBLeptIndex(3);          //-->Filled with which value in case only 4 jets are considered ... ?
-      std::cout << " bTagLeptIndex obtained from bTagStudy is : " << bTagLeptIndex << std::endl;
-  
+      float bTagLeptIndex = bTagStudy.getBLeptIndex(3);
+
+      //---------------------------------//
+      //  Decide on the event selection  //
+      //---------------------------------//
+      int ChosenBTag = 3;  //-->Corresponds to two T b-tags and no light veto!
+
+      //MSPlots with number of jets information before requiring at least two b-jets and at least 2 light jets!
+      MSPlot["nSelectedJets_BeforeBTag"+leptChannel]->Fill( selectedJets.size(),                           datasets[iDataSet], true, Luminosity*scaleFactor);
+      MSPlot["nBTaggedJets_BeforeBTag"+leptChannel]->Fill(  (bTagStudy.getbTaggedJets(ChosenBTag)).size(), datasets[iDataSet], true, Luminosity*scaleFactor);
+      MSPlot["nLightJets_BeforeBTag"+leptChannel]->Fill(    (bTagStudy.getLightJets(ChosenBTag)).size(),   datasets[iDataSet], true, Luminosity*scaleFactor);
+
+      //Apply the event selection
+      if( (bTagStudy.getbTaggedJets(ChosenBTag)).size() < 2 || (bTagStudy.getLightJets(ChosenBTag)).size() < 2 ) continue;
+
+      //Identical MSPlots with number of jets information after requiring at least two b-jets and at least 2 light jets!
+      MSPlot["nSelectedJets_AfterBTag"+leptChannel]->Fill( selectedJets.size(),                           datasets[iDataSet], true, Luminosity*scaleFactor);
+      MSPlot["nBTaggedJets_AfterBTag"+leptChannel]->Fill(  (bTagStudy.getbTaggedJets(ChosenBTag)).size(), datasets[iDataSet], true, Luminosity*scaleFactor);
+      MSPlot["nLightJets_AfterBTag"+leptChannel]->Fill(    (bTagStudy.getLightJets(ChosenBTag)).size(),   datasets[iDataSet], true, Luminosity*scaleFactor);      
+ 
+      //Write out the LHCO output!
+      //if( RecoLHCOOutput == true)
+      //  lhcoOutput.StoreRecoInfo(selectedLepton, selectedJets, bTagStudy.getBLeptIndex(ChosenBTag), bTagStudy.getBHadrIndex(ChosenBTag), bTagStudy.getLight1Index5Jets(ChosenBTag), bTagStudy.getLight2Index5Jets(ChosenBTag), decayChannel, LeptonRecoCharge, jetCombi); 
     }//End of loop on events
+
+    //--- Get output from bTagStudy class ---//
+    bTagStudy.ReturnBTagTable(dataSetName);
+    bTagStudy.CreateHistograms(outputFile);
+
+    //--- Get output from LHCOOutput class ---//
+    //lhcoOutput.WriteLHCOPlots(outputFile);	
 
   }//End of loop on datasets
 
+ /////////////////////////
+ // Write out the plots //
+ /////////////////////////
+ string pathPNG = "PlotsMacro";
+
+  outputFile -> cd();
+  mkdir((pathPNG+"/MSPlots").c_str(),0777);
+
+  TDirectory* msdir = outputFile->mkdir("MSPlots");
+  msdir->cd(); 
+  for(map<string,MultiSamplePlot*>::const_iterator it = MSPlot.begin(); it != MSPlot.end(); it++){    
+    MultiSamplePlot *temp = it->second;
+    string name = it->first;
+    temp->Draw(name, 0, false, false, false, 1);     //string label, unsigned int RatioType, bool addRatioErrorBand, bool addErrorBand, bool ErrorBandAroundTotalInput, int scaleNPSSignal 
+    temp->Write(outputFile, name, true, "PlotsMacro/MSPlots/", "pdf");
+  }
+
+  TDirectory* th1dir = outputFile->mkdir("1D_histograms");
+  th1dir->cd();
+  for(std::map<std::string,TH1F*>::const_iterator it = histo1D.begin(); it != histo1D.end(); it++){
+    TH1F *temp = it->second;
+    int N = temp->GetNbinsX();
+    temp->SetBinContent(N,temp->GetBinContent(N)+temp->GetBinContent(N+1));
+    temp->SetBinContent(N+1,0);
+    temp->SetEntries(temp->GetEntries()-2); // necessary since each SetBinContent adds +1 to the number of entries...
+    temp->Write();
+  }
+  TDirectory* th2dir = outputFile->mkdir("2D_histograms_graphs");
+  th2dir->cd();
+  for(std::map<std::string,TH2F*>::const_iterator it = histo2D.begin(); it != histo2D.end(); it++){
+    TH2F *temp = it->second;
+    temp->Write();
+  }
+
+  delete outputFile;
+
+  cout << "It took us " << ((double)clock() - start) / CLOCKS_PER_SEC << " to run the program" << endl;
+  cout << "********************************************" << endl;
+  cout << "           End of the program !!            " << endl;
+  cout << "           hasn't crashed yet ;-)           " << endl;
+  cout << "********************************************" << endl;
+  
+  return 0;
 }
- 
 
 /*
 	if(jetCombi[0]!=9999 && jetCombi[1]!=9999 && jetCombi[2]!=9999 && jetCombi[3]!=9999){    
@@ -151,35 +262,6 @@ int main (int argc, char *argv[])
 	  histo2D["MlbMqqbCorrectAll"]->Fill(MqqbCorrect,MlbCorrect);
         }
 
-      //----------------------------------------------------------------------------------------------------------------------------------- Start of bTagStudy class stuff!!      	
-      //---  Get the b-jet and light information for all six b-tag options!  ---//
-      bTagStudy.CalculateJets(selectedJets, jetCombi, selectedLepton);
-      float bTagLeptIndex = bTagStudy.getBLeptIndex(3);
-      //---------------------------------------------------------------------------------------------------------------------------- End of bTagStudy class stuff
-      
-      //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&//
-      //  Event selection choice (17/06/2014)  //
-      //   --> Continue with 2 T b-tags        //
-      //   --> No veto on light jets!          //
-      //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&//
-      ChosenBTag = 3;
-
-      //MSPlots with number of jets information before requiring at least two b-jets and at least 2 light jets!
-      MSPlot["nSelectedJets_BeforeBTag"+leptonFlav]->Fill(selectedJets.size(), datasets[d], true, Luminosity*scaleFactor);
-      MSPlot["nBTaggedJets_BeforeBTag"+leptonFlav]->Fill( (bTagStudy.getbTaggedJets(ChosenBTag)).size(), datasets[d], true, Luminosity*scaleFactor);
-      MSPlot["nLightJets_BeforeBTag"+leptonFlav]->Fill( (bTagStudy.getLightJets(ChosenBTag)).size(), datasets[d], true, Luminosity*scaleFactor);
-
-      //---  Require two b-jets and two light jets!  ---//
-      if( (bTagStudy.getbTaggedJets(ChosenBTag)).size() < 2 || (bTagStudy.getLightJets(ChosenBTag)).size() < 2 ){
-	if(RecoLHCOOutput == true) EventInfoFile<<"    B-tag failed "<<endl;
-	continue;
-      }      
-            
-      //Identical MSPlots with number of jets information after requiring at least two b-jets and at least 2 light jets!
-      MSPlot["nSelectedJets_AfterBTag"+leptonFlav]->Fill(selectedJets.size(), datasets[d], true, Luminosity*scaleFactor);
-      MSPlot["nBTaggedJets_AfterBTag"+leptonFlav]->Fill( (bTagStudy.getbTaggedJets(ChosenBTag)).size(), datasets[d], true, Luminosity*scaleFactor);
-      MSPlot["nLightJets_AfterBTag"+leptonFlav]->Fill( (bTagStudy.getLightJets(ChosenBTag)).size(), datasets[d], true, Luminosity*scaleFactor);
-      
       // Count the number of events:
       if(decayChannel == 0) nSelectedMu++;
       if(decayChannel == 1) nSelectedEl++;
@@ -200,10 +282,4 @@ int main (int argc, char *argv[])
     cout <<"\n values for Mlb :"<< histo1D["MlbMass"]->GetFunction("gaus")->GetParameter(1) << " +- " << histo1D["MlbMass"]->GetFunction("gaus")->GetParameter(2) << endl;
     cout <<" values for Mqqb :" << histo1D["MqqbMass"]->GetFunction("gaus")->GetParameter(1) << " +- " << histo1D["MqqbMass"]->GetFunction("gaus")->GetParameter(2) << endl;
 
-    //--- Get output from bTagStudy class ---//
-    bTagStudy.ReturnBTagTable();
-    bTagStudy.CreateHistograms(fout);
-
-    //--- Get output from LHCOOutput class ---//
-    lhcoOutput.WriteLHCOPlots(fout);	
 */
